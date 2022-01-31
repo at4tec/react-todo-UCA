451aa4fe07ba326b1d3a557cfbb78211977190bd
at4tec <at4tec@gmail.com>
2022-01-31 17:30:08 +0200

    Add comments and address items

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import React,{Component} from "react";
 import "./style.css";
 
-
+//-----------AddItem-------------
 class AddItem extends Component{
   render(){
     return <>Add Item</>
   }
 }
-
+//---------TodoItems------------
 const TodoItems =(props)=>{
   const {items}=props
   const ListItems=items.map(i=>{
     return (
+       
       <div>
+        
       <span>{i.name}</span>
       <span>{i.age}</span>
       <span>&times;</span>
 
       </div>
+      
     )
   })
-  return <div>{ListItems}</div>
+  return <>
+  <div>
+   <> Name</>
+   <> Age</>
+   <> Action</>
+
+  </div>
+  <div>{ListItems}</div>
+  </>
 }
 
-
+//------------App-----------
 
 export default class App extends Component {
   state ={
