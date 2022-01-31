e5f179c5f34a0222d16de7f92790c2f9f2d29ecd
at4tec <at4tec@gmail.com>
2022-01-31 03:18:00 +0200

    Create All Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React,{Component} from "react";
 import "./style.css";
 
-export default function App() {
+
+class AddItem extends Component{
+  render(){
+    return <>Add Item</>
+  }
+}
+
+
+const TodoItems =()=>{
+  return <div>List Items</div>
+}
+
+
+export default class App extends Component {
+  state ={
+    items:[
+      {name:'Aly',age:21},
+          {name:'Adel',age:24}
+        ]
+
+  }
+  render(){
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Todo</h1>
+      < TodoItems/>
+      <AddItem />
     </div>
   );
+}
 }
